839ee3d45fcf43fa87496f8b23ea9011b9ff2cff
jrsanchez061275 <jr.sanchez.alvarez061275@gmail.com>
2025-05-27 21:39:28 -0400

    Create estructura.sql

---
+++ estructura.sql
@@ -0,0 +1,24 @@
+-- Tabla de clientes
+CREATE TABLE clientes (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    correo VARCHAR(100)
+);
+
+-- Tabla de productos
+CREATE TABLE productos (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio DECIMAL(10,2)
+);
+
+-- Tabla de ventas
+CREATE TABLE ventas (
+    id INT PRIMARY KEY,
+    producto_id INT,
+    cliente_id INT,
+    fecha_venta DATE,
+    monto_venta DECIMAL(10,2),
+    FOREIGN KEY (producto_id) REFERENCES productos(id),
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
